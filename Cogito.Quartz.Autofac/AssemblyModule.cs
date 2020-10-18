ce33eae0f293fb12610396134b3d3871f7091027
Jerome Haltom <jhaltom@alethic.solutions>
2020-10-18 16:30:33 -0500

    Update base to Autofac 6

--- Cogito.Quartz.Autofac/AssemblyModule.cs
+++ Cogito.Quartz.Autofac/AssemblyModule.cs
@@ -1,17 +1,16 @@
 ﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Autofac.Extras.Quartz;
 
-
 using Cogito.Autofac;
+using Cogito.Autofac.DependencyInjection;
 using Cogito.Extensions.Options.Autofac;
 using Cogito.Extensions.Options.Configuration.Autofac;
-using Cogito.Quartz.Hosting;
-using Cogito.Quartz.Options;
 
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 using Quartz;
+using Quartz.Logging;
 
 namespace Cogito.Quartz.Autofac
 {
@@ -26,9 +25,9 @@ namespace Cogito.Quartz.Autofac
         {
             builder.RegisterFromAttributes(typeof(AssemblyModule).Assembly);
             builder.RegisterModule(new QuartzAutofacFactoryModule() { ConfigurationProvider = ctx => ctx.Resolve<QuartzConfigurationProvider>().Build() });
-            builder.Configure<QuartzOptions>("Quartz");
+            builder.Configure<Cogito.Quartz.Options.QuartzOptions>("Quartz");
             builder.Configure<QuartzHostedServiceOptions>("Quartz:Host");
-            builder.RegisterType<QuartzHostedService>().As<IHostedService>();
+            builder.Populate(s => s.AddQuartzHostedService());
             builder.RegisterBuildCallback(ctx => LogContext.SetCurrentLogProvider(ctx.Resolve<ILoggerFactory>()));
         }
 
